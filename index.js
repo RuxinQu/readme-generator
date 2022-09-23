372268e65b1b27ab6b9670776dbb890023988c71
Ruxin Qu <rosheenqu@gmail.com>
2022-09-22 23:34:16 -0700

    answer the prompt question and a README.md file is generated

--- index.js
+++ index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What\'s the title of your project?',
+        validate: function (input) {
+            if (!input) {
+                return 'Please enter the title of your project!'
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide the description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions on how to use this project:'
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Please provide the name of the screenshot in the assets/images folder'
+    },
+    {
+        type: 'input',
+        name: 'credit',
+        message: 'Please list the github username of your collaborator(s) (if any)'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Enter project license:',
+        choices: [
+            "APACHE 2.O",
+            "BSD 3",
+            "GVL-GPL 3.0",
+            "MIT",
+            "None"
+        ],
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How users can contribute to your project (if any).'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide tests for project, and explain how to test (if any).',
+    },
+    {
+        type: 'input',
+        message: 'Enter your github username:',
+        name: 'username'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('File successfully written to disk');
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const result = await inquirer.prompt(questions);
+    writeToFile('exampleREADME.md', generateMarkdown(result))
+
+}
 
 // Function call to initialize app
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let markdown = [];
+  markdown.push('# ' + data.title);
 
-`;
+  const description = '## Description';
+  markdown.push(`${description}\n\n${data.description}`);
+
+  const tableOfContents = '## Table of Contents';
+  const tableList1 = '- [Installation](#installation)';
+  const tableList2 = '- [Usage](#usage)';
+  const tableList3 = '- [Credits](#credits)';
+  const tableList4 = '- [License](#license)';
+  markdown.push(`${tableOfContents}\n\n${tableList1}\n${tableList2}\n${tableList3}\n${tableList4}`);
+
+  const installation = '## Installation'
+  markdown.push(`${installation}\n\n${data.installation}`)
+
+  const usage = '## Usage';
+  const usageText = data.usage;
+  const screenshotDescription = 'Here\'s a screenshot of the project'
+  const screenshot = `![project screenshot](assets/images/${data.screenshot})`;
+  markdown.push(`${usage}\n\n${usageText}\n\n${screenshotDescription}\n\n${screenshot}`)
+
+  if (data.credit) {
+    const credit = '## Credit';
+    markdown.push(`${credit}\n\n${data.credit}`);
+  }
+
+  //license + bages
+  if (data.contribute) {
+    const contribute = '## Contribute';
+    markdown.push(`${contribute}\n\n${data.contribute}`);
+  }
+
+  if (data.test) {
+    const test = '## Tests';
+    markdown.push(`${test}\n\n${data.test}`);
+  }
+
+  return markdown.join('\n\n');
 }
 
 module.exports = generateMarkdown;
